8523d4c3b1f8cdc2c1b01f7ed5c6f3ae2de08d6b
UsnikB <usnikbiswas.work@gmail.com>
2022-08-06 01:26:51 -0400

    After Fist Execution, Let it runn till Positive mean

--- pg-pong.py
+++ pg-pong.py
@@ -9,7 +9,7 @@ batch_size = 10 # every how many episodes to do a param update?
 learning_rate = 1e-4
 gamma = 0.99 # discount factor for reward
 decay_rate = 0.99 # decay factor for RMSProp leaky sum of grad^2
-resume = False # resume from previous checkpoint?
+resume = True # resume from previous checkpoint?
 render = False
 
 # model initialization
